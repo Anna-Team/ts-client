f2ace68f7368c7986058e46c85b080b025b9a355
Anthony Master <anthony.master@ifbmt.info>
2022-10-10 22:45:20 -0500

    remove unused function

--- src/encode/encodeFieldNames.ts
+++ src/encode/encodeFieldNames.ts
@@ -1,7 +1,3 @@
 export const encodeFieldName = (field: string) => {
   return `value|${field}|`
 }
-
-export const encodeFieldNames = (fields: string[]) => {
-  return fields.map((field) => encodeFieldName(field)).join(',')
-}
